c338cd9d8720d935769794192165af60fe0e6fd2
Your Name <yudhaxsinatra@gmail.com>
2022-11-21 01:35:55 +0700

    Task Screen, Bottomsheet

--- lib/app/modules/task/views/task_view.dart
+++ lib/app/modules/task/views/task_view.dart
@@ -31,7 +31,7 @@ class TaskView extends GetView<TaskController> {
                 !context.isPhone
                     ? const Header()
                     : Container(
-                        padding: EdgeInsets.all(20),
+                        padding: const EdgeInsets.all(20),
                         child: Row(
                           children: [
                             IconButton(
@@ -64,7 +64,7 @@ class TaskView extends GetView<TaskController> {
                                 ),
                               ],
                             ),
-                            Spacer(),
+                            const Spacer(),
                             const Icon(Icons.notifications,
                                 color: AppColors.primaryText, size: 30),
                             const SizedBox(
@@ -86,14 +86,123 @@ class TaskView extends GetView<TaskController> {
                 Expanded(
                   child: Container(
                     padding: !context.isPhone
-                        ? EdgeInsets.all(10)
-                        : EdgeInsets.all(0),
+                        ? const EdgeInsets.all(20)
+                        : const EdgeInsets.all(20),
                     margin: !context.isPhone
-                        ? EdgeInsets.all(10)
-                        : EdgeInsets.all(0),
+                        ? const EdgeInsets.all(10)
+                        : const EdgeInsets.all(0),
                     decoration: BoxDecoration(
                       color: Colors.blue[100],
                       borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "My Tasks",
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: AppColors.secondaryText,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Expanded(
+                          child: ListView.builder(
+                            clipBehavior: Clip.antiAlias,
+                            shrinkWrap: true,
+                            itemCount: 5,
+                            itemBuilder: ((context, index) {
+                              return Container(
+                                height: 200,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  color: Colors.white,
+                                ),
+                                margin: const EdgeInsets.all(10),
+                                padding: const EdgeInsets.all(20),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(30),
+                                          child: const CircleAvatar(
+                                            backgroundColor: Colors.amber,
+                                            radius: 20,
+                                            foregroundImage: NetworkImage(
+                                                "https://cdn.mos.cms.futurecdn.net/XDLmYsaAh4xF2yVzqVZPva.jpg"),
+                                          ),
+                                        ),
+                                        ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(30),
+                                          child: const CircleAvatar(
+                                            backgroundColor: Colors.amber,
+                                            radius: 20,
+                                            foregroundImage: NetworkImage(
+                                                "https://cdn.mos.cms.futurecdn.net/XDLmYsaAh4xF2yVzqVZPva.jpg"),
+                                          ),
+                                        ),
+                                        ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(30),
+                                          child: const CircleAvatar(
+                                            backgroundColor: Colors.amber,
+                                            radius: 20,
+                                            foregroundImage: NetworkImage(
+                                                "https://cdn.mos.cms.futurecdn.net/XDLmYsaAh4xF2yVzqVZPva.jpg"),
+                                          ),
+                                        ),
+                                        const Spacer(),
+                                        Container(
+                                          height: 25,
+                                          width: 80,
+                                          color: AppColors.primaryBg,
+                                          child: const Center(
+                                            child: Text(
+                                              "100%",
+                                              style: TextStyle(
+                                                  color: AppColors.primaryText),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    const Spacer(),
+                                    Container(
+                                      height: 25,
+                                      width: 80,
+                                      color: AppColors.primaryBg,
+                                      child: const Center(
+                                        child: Text(
+                                          "10/10 Tasks",
+                                          style: TextStyle(
+                                              color: AppColors.primaryText),
+                                        ),
+                                      ),
+                                    ),
+                                    const Text(
+                                      "Flutter Management",
+                                      style: TextStyle(
+                                          color: AppColors.secondaryText,
+                                          fontSize: 20),
+                                    ),
+                                    const Text(
+                                      "Deadline 20/10/2021",
+                                      style: TextStyle(
+                                          color: AppColors.secondaryText),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            }),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
@@ -101,6 +210,30 @@ class TaskView extends GetView<TaskController> {
             ),
           ),
         ],
+      ),
+      floatingActionButton: Align(
+        alignment: const Alignment(.9, .9),
+        child: FloatingActionButton.extended(
+          onPressed: () {
+            Get.bottomSheet(
+              Container(
+                margin: !context.isPhone
+                    ? EdgeInsets.only(left: 100, right: 100)
+                    : EdgeInsets.zero,
+                height: Get.height,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                  color: Colors.white,
+                ),
+              ),
+            );
+          },
+          label: const Text("Add Task"),
+          icon: const Icon(Icons.add),
+        ),
       ),
     );
   }
